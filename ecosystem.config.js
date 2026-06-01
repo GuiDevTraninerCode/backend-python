6c1b6265568311b278b2e8b02025512edd5634ee
guimartins05 <guimartins20@gmail.com>
2026-06-01 19:21:43 -0300

    API de Teste v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,10 @@ module.exports = {
     {
       name: "backend-python",
       // Caminho para o executável do gunicorn ou uvicorn dentro do ambiente virtual da VM
-      script: "/home/SEU_USUARIO_GCP/apps/python/venv/bin/gunicorn",
+      script: "/home/guimartins20/apps/python/venv/bin/gunicorn",
       // Ajuste 'main:app' para o nome do seu arquivo principal e da sua instância da API (ex: fastapi ou flask)
       args: "main:app --workers 2 --bind 127.0.0.1:8000",
-      cwd: "/home/SEU_USUARIO_GCP/apps/python",
+      cwd: "/home/guimartins20/apps/python",
       autorestart: true,
       watch: false
     }
