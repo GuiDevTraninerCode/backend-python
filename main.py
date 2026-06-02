03210ca7a3b440dbd348c32b47a9a3d2b2753890
guimartins05 <guimartins20@gmail.com>
2026-06-02 16:43:16 -0300

    Teste ssh

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Item(BaseModel):
 # 1. Rota Inicial (Home)
 @app.get("/")
 def home():
-    return {"mensagem": "Back-end Python!"}
+    return {"mensagem": "Back-end Python 2!"}
 
 # 2. Rota para Listar todos os itens (GET)
 @app.get("/tarefas", response_model=List[dict])
