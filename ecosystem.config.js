b6e51d397ba84e3cf95d3cef53e1181c5724d5c9
guimartins05 <guimartins20@gmail.com>
2026-06-02 10:30:33 -0300

    API de Teste v5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
   apps: [
     {
       name: "backend-python",
-      // Caminho para o executável do gunicorn ou uvicorn dentro do ambiente virtual da VM
-      script: "/home/guimartins20/apps/python/venv/bin/unicorn",
-      // Ajuste 'main:app' para o nome do seu arquivo principal e da sua instância da API (ex: fastapi ou flask)
-      args: "main:app --workers 2 --bind 127.0.0.1:8000",
+      // CORREÇÃO: Adicionado o 'v' em uvicorn
+      script: "/home/guimartins20/apps/python/venv/bin/uvicorn",
+      // CORREÇÃO: Argumentos padrão do Uvicorn para o FastAPI funcionar em ASGI
+      args: "main:app --host 127.0.0.1 --port 8000 --workers 2",
       cwd: "/home/guimartins20/apps/python",
-      interpreter: "none",       
+      interpreter: "none",      
       exec_mode: "fork",      
       autorestart: true,
       watch: false
